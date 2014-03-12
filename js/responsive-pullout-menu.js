fb7163426f8f817cc6d31ce1449afeb4858043ee
Rashaad Ramdeen <rashaad.ramdeen@gmail.com>
2014-03-12 19:24:43 -0400

    fixed menu button scroll issue

--- js/responsive-pullout-menu.js
+++ js/responsive-pullout-menu.js
@@ -7,12 +7,13 @@ $(function(){
 		if(position == 0){
 			$('nav#left').animate({"left": '+=240px'}, speed);
 			$('#wrapper').animate({"left": '+=240px'}, speed);
+			$('#responsive-btn').animate({"left": '+=240px'}, speed);
 			position = 1;
 		}else{
 			$('nav#left').animate({"left": '-=240px'}, speed);
 			$('#wrapper').animate({"left": '-=240px'}, speed);
+			$('#responsive-btn').animate({"left": '-=240px'}, speed);
 			position = 0;
 		}
-
 	});
 });
