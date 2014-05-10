67066d662a0c463d85908616744a790e6a0b4b10
Rashaad Ramdeen <rashaad.ramdeen@gmail.com>
2014-05-10 10:07:22 -0400

    fixed offset and page height issue

--- js/parallax.js
+++ js/parallax.js
@@ -6,7 +6,7 @@
 
 var Parallax = (function($){
 
-	function Parallax(el) {
+	function Parallax(el, window_height, overhang) {
 		this.el = el;
 		this.sections = $(el);
 		this.top = $(window).scrollTop();
@@ -17,11 +17,13 @@ var Parallax = (function($){
 		this.offset;
 		this.offsets;
 		this.scroll;
-		this.top;
 		this.transform;
+		this.distance_scrolled; //window height + scrollTop
+		this.window_height = window_height;
+		this.overhang = overhang;
 
 		//compute offsets of each section from top using a mapping function
-		this.offsets = this.sections.get().map(function(div,d) {
+		this.offsets = this.sections.get().map(function(div,j) {
 			return $(div).offset();
 		});
 
@@ -55,22 +57,28 @@ var Parallax = (function($){
 		var render = function() {
 
 			self.top = $(window).scrollTop();
+			self.distance_scrolled = self.top + $(window).height();
 
 			for(self.i = 0; self.i < self.len; self.i++) {
 
-				self.div = self.sections[self.i];
+				//check if the slide is visible, only then we should parallax
+				if(self.distance_scrolled > self.offsets[self.i].top && self.distance_scrolled < (self.offsets[self.i].top + $(window).height() + self.window_height)) {
+					self.div = self.sections[self.i];
 
-				self.offset = self.top - self.offsets[self.i].top;
+					self.offset = self.top - self.offsets[self.i].top;
 
-				self.scroll = ~~((self.offset / 600) * 300);
+					self.scroll = ~~((self.offset / self.window_height) * self.overhang);
 
-				self.transform = 'translate3d(0px, ' + self.scroll + 'px, 0px)';
+					self.transform = 'translate3d(0px, ' + self.scroll + 'px, 0px)';
 
-				self.div.style.webkitTransform = self.transform;
-				self.div.style.MozTransform = self.transform
-				self.div.style.msTransform = self.transform;
-				self.div.style.OTransform = self.transform;
-				self.div.style.transform = self.transform;
+					self.div.style.webkitTransform = self.transform;
+					self.div.style.MozTransform = self.transform
+					self.div.style.msTransform = self.transform;
+					self.div.style.OTransform = self.transform;
+					self.div.style.transform = self.transform;
+				}
+
+				
 			}
 		};
 
